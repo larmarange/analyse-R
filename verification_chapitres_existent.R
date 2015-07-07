d064839ff5d0093bc502ebde604b2f68f3ed2cca
François <briatte@gmail.com>
2015-07-07 19:38:06 +0200

    script pour checker les liens entre chapitres

---
+++ verification_chapitres_existent.R
@@ -0,0 +1,13 @@
+# Trouver tous les liens vers des pages HTML fixes
+
+p <- c()
+for (f in list.files(pattern = "Rmd$")) {
+  t <- readLines(f)
+  p <- unique(c(p, t[ grepl("\\[(.*)\\]\\([a-z0-9_]+\\.html\\)", t) ]))
+
+}
+p <- gsub("(.*)\\(([a-z0-9_]+\\.html)\\)(.*)", "\\2", p)
+p <- sort(p[ !file.exists(p) ])
+
+if(length(p))
+  warning(length(p), " liens pointent vers des chapitres inexistants.")
