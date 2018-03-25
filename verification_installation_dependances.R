6c4cea025a31ce87200d2d0c5ad670ebfac31d8f
Joseph Larmarange <joseph@larmarange.net>
2018-03-25 16:44:28 +0200

    attention si quietly = TRUE dans library

--- verification_installation_dependances.R
+++ verification_installation_dependances.R
@@ -13,6 +13,8 @@ for (f in list.files(pattern = "Rmd$")) {
 }
 p <- gsub("library|require|\\(|\\)", "", p)
 p <- gsub("\"", "", p)
+p <- gsub(", quietly = TRUE", "", p)
+p <- unique(p)
 m <- p[!p %in% installed.packages()[, 1]]
 
 if (!length(m)) {
