fc15c2556e3063c6a771f154b66d188a4c79f21d
Joseph Larmarange <joseph@larmarange.net>
2022-09-10 08:42:14 +0200

    script pour faciliter conversion qmd

---
+++ rmd_to_qmd.R
@@ -0,0 +1,39 @@
+# Conversion Rmd vers qmd
+
+library(tidyverse)
+
+fichiers <- list.files(pattern = ".Rmd")
+fichiers <- fichiers[fichiers != 'index_fonctions.Rmd']
+fichiers <- fichiers[fichiers != 'index_extensions.Rmd']
+fichiers <- fichiers[fichiers != 'index_concepts.Rmd']
+
+clean <- function(f) {
+  content <- readLines(f, encoding = "UTF-8")
+
+  # fonctions
+  pattern_fonctions <- '`([a-zA-Z0-9._%]+)`[{]data-pkg="([a-zA-Z0-9._]+)"[^}]*[}]'
+  content <- str_replace_all(
+    content,
+    pattern_fonctions,
+    "`\\2::\\1()`"
+  )
+
+  # packages
+  pattern_packages <- '`([a-zA-Z0-9._%]+)`\\{\\.pkg\\}'
+  content <- str_replace_all(
+    content,
+    pattern_packages,
+    "`{\\1}`"
+  )
+
+  # dfn
+  pattern_dfn <- '</?dfn[^>]*>'
+  content <- str_remove_all(content, pattern_dfn)
+
+  f2 <- paste0("qmd/", str_sub(f, 1, -5), ".qmd")
+  writeLines(content, f2, useBytes = TRUE)
+}
+
+for (f in fichiers) {
+  clean(f)
+}
